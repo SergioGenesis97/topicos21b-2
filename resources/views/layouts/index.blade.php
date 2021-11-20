c940a4d3a8a6818ad516bed3d5d0161663bd8594
SergioGenesis97 <sergio.bautista@alumnos.udg.mx>
2021-11-19 21:31:22 -0600

    Cambios en diseño

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -17,6 +17,8 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
     <!-- Theme style -->
     <link rel="stylesheet" href="dist/css/adminlte.min.css">
+
+    <link rel="stylesheet" href="dist/css/estilos.css">
     
     
 </head>
@@ -25,14 +27,14 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <div class="wrapper">
 
         <!-- Navbar -->
-        <nav class="main-header navbar navbar-expand navbar-black navbar-dark">
+        <nav class="main-header navbar navbar-expand navbar-black" style="background-color: rgba(0, 0, 0, 0.860)">
             <!-- Left navbar links -->
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+                    <a class="nav-link" data-widget="pushmenu" href="#" role="button"  id="text-blue"><i class="fas fa-bars"></i></a>
                 </li>
                 <li class="nav-item d-none d-sm-inline-block">
-                    <a href="/home" class="nav-link">Home</a>
+                    <a href="/home" class="nav-link" id="text-blue">Home</a>
                 </li>
             </ul>
 
@@ -40,13 +42,13 @@ scratch. This page gets rid of all links and provides the needed markup only.
             <ul class="navbar-nav ml-auto">
                 <!-- Navbar Search -->
                 <li class="nav-item">
-                    <a class="nav-link" data-widget="navbar-search" href="#" role="button">
+                    <a title="Buscar" class="nav-link"  id="text-blue" data-widget="navbar-search" href="#" role="button">
                         <i class="fas fa-search"></i>
                     </a>
                     <div class="navbar-search-block">
                         <form class="form-inline">
                             <div class="input-group input-group-sm">
-                                <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+                                <input class="form-control form-control-navbar" type="search" placeholder="Buscar..." aria-label="Search">
                                 <div class="input-group-append">
                                     <button class="btn btn-navbar" type="submit">
                                         <i class="fas fa-search"></i>
@@ -59,108 +61,12 @@ scratch. This page gets rid of all links and provides the needed markup only.
                         </form>
                     </div>
                 </li>
-
-                <!-- Messages Dropdown Menu -->
-                <li class="nav-item dropdown">
-                    <a class="nav-link" data-toggle="dropdown" href="#">
-                        <i class="far fa-comments"></i>
-                        <span class="badge badge-danger navbar-badge">3</span>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-                        <a href="#" class="dropdown-item">
-                            <!-- Message Start -->
-                            <div class="media">
-                                <img src="dist/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle">
-                                <div class="media-body">
-                                    <h3 class="dropdown-item-title">
-                                        Brad Diesel
-                                        <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
-                                    </h3>
-                                    <p class="text-sm">Call me whenever you can...</p>
-                                    <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-                                </div>
-                            </div>
-                            <!-- Message End -->
-                        </a>
-                        <div class="dropdown-divider"></div>
-                        <a href="#" class="dropdown-item">
-                            <!-- Message Start -->
-                            <div class="media">
-                                <img src="dist/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
-                                <div class="media-body">
-                                    <h3 class="dropdown-item-title">
-                                        John Pierce
-                                        <span class="float-right text-sm text-muted"><i class="fas fa-star"></i></span>
-                                    </h3>
-                                    <p class="text-sm">I got your message bro</p>
-                                    <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-                                </div>
-                            </div>
-                            <!-- Message End -->
-                        </a>
-                        <div class="dropdown-divider"></div>
-                        <a href="#" class="dropdown-item">
-                            <!-- Message Start -->
-                            <div class="media">
-                                <img src="dist/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
-                                <div class="media-body">
-                                    <h3 class="dropdown-item-title">
-                                        Nora Silvester
-                                        <span class="float-right text-sm text-warning"><i class="fas fa-star"></i></span>
-                                    </h3>
-                                    <p class="text-sm">The subject goes here</p>
-                                    <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-                                </div>
-                            </div>
-                            <!-- Message End -->
-                        </a>
-                        <div class="dropdown-divider"></div>
-                        <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
-                    </div>
-                </li>
-                <!-- Notifications Dropdown Menu -->
-                <li class="nav-item dropdown">
-                    <a class="nav-link" data-toggle="dropdown" href="#">
-                        <i class="far fa-bell"></i>
-                        <span class="badge badge-warning navbar-badge">15</span>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-                        <span class="dropdown-header">15 Notifications</span>
-                        <div class="dropdown-divider"></div>
-                        <a href="#" class="dropdown-item">
-                            <i class="fas fa-envelope mr-2"></i> 4 new messages
-                            <span class="float-right text-muted text-sm">3 mins</span>
-                        </a>
-                        <div class="dropdown-divider"></div>
-                        <a href="#" class="dropdown-item">
-                            <i class="fas fa-users mr-2"></i> 8 friend requests
-                            <span class="float-right text-muted text-sm">12 hours</span>
-                        </a>
-                        <div class="dropdown-divider"></div>
-                        <a href="#" class="dropdown-item">
-                            <i class="fas fa-file mr-2"></i> 3 new reports
-                            <span class="float-right text-muted text-sm">2 days</span>
-                        </a>
-                        <div class="dropdown-divider"></div>
-                        <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
-                    </div>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" data-widget="fullscreen" href="#" role="button">
-                        <i class="fas fa-expand-arrows-alt"></i>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
-                        <i class="fas fa-th-large"></i>
-                    </a>
-                </li>
             </ul>
         </nav>
         <!-- /.navbar -->
 
         <!-- Main Sidebar Container -->
-        <aside class="main-sidebar sidebar-dark-primary elevation-4">
+        <aside class="main-sidebar sidebar-dark-primary elevation-4" id="sidebar-img">
             <!-- Brand Logo -->
             <a href="/home" class="brand-link">
                 <img src="dist/img/task.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
@@ -182,10 +88,10 @@ scratch. This page gets rid of all links and provides the needed markup only.
                 <!-- SidebarSearch Form -->
                 <div class="form-inline">
                     <div class="input-group" data-widget="sidebar-search">
-                        <input class="form-control form-control-sidebar" type="search" placeholder="Buscar..." aria-label="Search">
+                        <input class="form-control form-control-sidebar" type="search" placeholder="Buscar..." aria-label="Search" >
                         <div class="input-group-append">
                             <button class="btn btn-sidebar">
-                                <i class="fas fa-search fa-fw"></i>
+                                <i class="fas fa-search fa-fw  text-warning"></i>
                             </button>
                         </div>
                     </div>
@@ -193,7 +99,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
                 <!-- Sidebar Menu -->
                 <nav class="mt-2">
-                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                    <ul id="text-dark" class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                         <!-- Add icons to the links using the .nav-icon class
                             with font-awesome or any other icon font library -->
                         <!--   USUARIOS    -->
@@ -207,13 +113,13 @@ scratch. This page gets rid of all links and provides the needed markup only.
                             </a>
                             <ul class="nav nav-treeview">
                                 <li class="nav-item">
-                                    <a href="/usuarios_create" class="nav-link bg-dark">
+                                    <a href="/usuarios_create" class="nav-link">
                                         <i class="material-icons nav-icon">person_add</i>
                                         <p>Agregar Usuarios</p>
                                     </a>
                                 </li>
                                 <li class="nav-item">
-                                    <a href="/usuarios_show" class="nav-link bg-dark">
+                                    <a href="/usuarios_show" class="nav-link">
                                         <i class="material-icons nav-icon">face</i>
                                         <p>Mostrar Usuarios</p>
                                     </a>
@@ -231,13 +137,13 @@ scratch. This page gets rid of all links and provides the needed markup only.
                             </a>
                             <ul class="nav nav-treeview">
                                 <li class="nav-item">
-                                    <a href="/productos_create" class="nav-link bg-dark">
+                                    <a href="/productos_create" class="nav-link">
                                         <i class="material-icons nav-icon">add</i>
                                         <p>Agregar Productos</p>
                                     </a>
                                 </li>
                                 <li class="nav-item">
-                                    <a href="/productos_show" class="nav-link bg-dark">
+                                    <a href="/productos_show" class="nav-link">
                                         <i class="material-icons nav-icon">visibility</i>
                                         <p>Mostrar Productos</p>
                                     </a>
@@ -255,13 +161,13 @@ scratch. This page gets rid of all links and provides the needed markup only.
                             </a>
                             <ul class="nav nav-treeview">
                                 <li class="nav-item">
-                                    <a href="/proveedores_create" class="nav-link bg-dark">
+                                    <a href="/proveedores_create" class="nav-link">
                                         <i class="material-icons nav-icon">add</i>
                                         <p>Agregar Proveedores</p>
                                     </a>
                                 </li>
                                 <li class="nav-item">
-                                    <a href="/proveedores_show" class="nav-link bg-dark">
+                                    <a href="/proveedores_show" class="nav-link">
                                         <i class="material-icons nav-icon">visibility</i>
                                         <p>Mostrar Proveedores</p>
                                     </a>
@@ -279,13 +185,13 @@ scratch. This page gets rid of all links and provides the needed markup only.
                             </a>
                             <ul class="nav nav-treeview">
                                 <li class="nav-item">
-                                    <a href="/clientes_create" class="nav-link bg-dark">
+                                    <a href="/clientes_create" class="nav-link">
                                         <i class="material-icons nav-icon">group_add</i>
                                         <p>Agregar Clientes</p>
                                     </a>
                                 </li>
                                 <li class="nav-item">
-                                    <a href="/clientes_show" class="nav-link bg-dark">
+                                    <a href="/clientes_show" class="nav-link">
                                         <i class="material-icons nav-icon">visibility</i>
                                         <p>Mostrar Clientes</p>
                                     </a>
@@ -338,13 +244,13 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <!-- /.control-sidebar -->
 
         <!-- Main Footer -->
-        <footer class="main-footer">
+        <footer class="main-footer" style="background-color: rgba(0, 0, 0, 0.870)">
             <!-- To the right -->
             <div class="float-right d-none d-sm-inline">
                 Todo lo que buscas
             </div>
             <!-- Default to the left -->
-            <strong>Copyright &copy; 2021-2032 <a href="#">SR</a>.</strong> Todos los derechos reservados.
+            Copyright &copy; 2021-2032 <a href="#" class="text-danger">SR</a>. Todos los derechos reservados.
         </footer>
     </div>
     <!-- ./wrapper -->
